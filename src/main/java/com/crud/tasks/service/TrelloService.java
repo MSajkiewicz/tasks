ff5918dd89f5ad8fbaa66dc6277cf7fa11384063
MSajkiewicz <marcinsajkiewicz91@gmail.com>
2020-01-19 15:07:55 +0100

    21.1 Stosowanie wzorca fasady

--- src/main/java/com/crud/tasks/service/TrelloService.java
+++ src/main/java/com/crud/tasks/service/TrelloService.java
@@ -3,7 +3,7 @@ package com.crud.tasks.service;
 import com.crud.tasks.config.AdminConfig;
 import com.crud.tasks.domain.Mail;
 import com.crud.tasks.trello.client.TrelloClient;
-import com.crud.tasks.trello.domain.CreatedTrelloCard;
+import com.crud.tasks.trello.domain.CreatedTrelloCardDto;
 import com.crud.tasks.trello.domain.TrelloBoardDto;
 import com.crud.tasks.trello.domain.TrelloCardDto;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,8 +30,8 @@ public class TrelloService {
         return trelloClient.getTrelloBoards();
     }
 
-    public CreatedTrelloCard createTrelloCard(final TrelloCardDto trelloCardDto) {
-        CreatedTrelloCard newCard = trelloClient.createNewCard(trelloCardDto);
+    public CreatedTrelloCardDto createTrelloCard(final TrelloCardDto trelloCardDto) {
+        CreatedTrelloCardDto newCard = trelloClient.createNewCard(trelloCardDto);
         ofNullable(newCard).ifPresent(card -> emailService.send(new Mail(adminConfig.getAdminMail(), null, SUBJECT,
                 "New card: " + card.getName() + " has been created on your Trello account")));
         return newCard;
