e22d5b7698017ccbdae73b0b95b46dea51ab0cb0
MSajkiewicz <marcinsajkiewicz91@gmail.com>
2020-01-22 18:24:36 +0100

    21.2

--- src/test/java/com/crud/tasks/scheduler/EmailSchedulerTest.java
+++ src/test/java/com/crud/tasks/scheduler/EmailSchedulerTest.java
@@ -1,18 +1,24 @@
 package com.crud.tasks.scheduler;
 
 import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.domain.Mail;
 import com.crud.tasks.repository.TaskRepository;
 import com.crud.tasks.service.SimpleEmailService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import static junit.framework.TestCase.assertNull;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class EmailSchedulerTest {
+
     @InjectMocks
     private EmailScheduler emailScheduler;
 
@@ -20,21 +26,46 @@ public class EmailSchedulerTest {
     private SimpleEmailService simpleEmailService;
 
     @Mock
-    private AdminConfig adminConfig;
-
-    @Mock
     private TaskRepository taskRepository;
 
+    @Mock
+    private AdminConfig adminConfig;
+
+    @Captor
+    private ArgumentCaptor<Mail> mailArgumentCaptor;
+
     @Test
-    public void sendInformationEmailTest() {
+    public void sendInformationEmailTestWithOneTask() {
         //Given
         when(adminConfig.getAdminMail()).thenReturn("mail@mail.com");
-        when(taskRepository.count()).thenReturn((long) 1);
+        when(taskRepository.count()).thenReturn(1L);
 
         //When
         emailScheduler.sendInformationEmail();
 
         //Then
-        verify(simpleEmailService, times(1)).send(any());
+        verify(simpleEmailService, times(1)).send(mailArgumentCaptor.capture());
+        Mail captoredMail = mailArgumentCaptor.getValue();
+        assertEquals("Currently in database You have 1 task", captoredMail.getMessage());
+        assertEquals("mail@mail.com", captoredMail.getMailTo());
+        assertEquals("Tasks: once a day email", captoredMail.getSubject());
+        assertNull(captoredMail.getToCc());
+    }
+    @Test
+    public void sendInformationEmailTestWithTwoTasks() {
+        //Given
+        when(adminConfig.getAdminMail()).thenReturn("mail@mail.com");
+        when(taskRepository.count()).thenReturn(2L);
+
+        //When
+        emailScheduler.sendInformationEmail();
+
+        //Then
+        verify(simpleEmailService, times(1)).send(mailArgumentCaptor.capture());
+        Mail captoredMail = mailArgumentCaptor.getValue();
+        assertEquals("Currently in database You have 2 tasks", captoredMail.getMessage());
+        assertEquals("mail@mail.com", captoredMail.getMailTo());
+        assertEquals("Tasks: once a day email", captoredMail.getSubject());
+        assertNull(captoredMail.getToCc());
     }
 }
